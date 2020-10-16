e410e2db104f93e0efe72c5cc9126834636fb8d8
Bavithra Ravichandran <ravibavi1967@gmail.com>
2020-10-16 12:52:06 +0530

    Iteration2

--- chaseTarget.js
+++ chaseTarget.js
@@ -9,6 +9,7 @@ rl.on('line', (line) => {
     var arr = [];
     var nums = line.split(' ');
     var target = parseInt(nums[0]);
+    var overs = parseInt(nums[1]);
     chaseTarget(target);
 });
 
@@ -17,30 +18,44 @@ function getrandomScore(min, max) {
 }
 
 class Match {
- constructor(target){
-    this.target = target;
-}
-singleOverScore(){
-    let score = 0;
-    for (let i = 0; i < 6; i++) {
-        let eachBallScore = getrandomScore(0, 7);
-        console.log(eachBallScore)
-        score += eachBallScore;
-        if (score >= this.target) {
-            return score;
-        }
+    constructor(target, overs) {
+        this.target = target;
+        this.overs = overs;
     }
+
+    balls = function getNumberBalls(overs) {
+        return overs * 6;
+    }
+    getScore(overs) {
+        let score = 0;
+        let totalBalls = this.balls(overs);
+        for (let i = 0; i < totalBalls; i++) {
+            let batsmanScore = getrandomScore(0, 7);
+            let bowlerScore = getrandomScore(0, 7);
+            console.log("Batsman Score:", batsmanScore)
+            console.log("Bowler Score:", bowlerScore)
+            if (batsmanScore === bowlerScore) {
+                return false;
+            }
+            score += batsmanScore;
+            if (score >= this.target) {
+                return score;
+            }
+        }
         return score;
-}
+    }
+
 }
 
 function chaseTarget(target) {
-        let match = new Match(target);
-        let scoreForover=  match.singleOverScore();
-        if (scoreForover >= target) {
-            console.log("Batsman won");
-        } else {
-            console.log("Batsman loss");
-        }  
+    let overs = 2;
+    let match = new Match(target, overs);
+
+    let scoreForover = match.getScore(overs);
+    if (scoreForover >= target) {
+        console.log("Batsman won");
+    } else {
+        console.log("Batsman loss");
+    }
 
 }
