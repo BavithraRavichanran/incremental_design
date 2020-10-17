aa2f7292d96538f42e98b5908130d1ef57c38c7c
Bavithra Ravichandran <ravibavi1967@gmail.com>
2020-10-17 13:12:24 +0530

    Iteration 5 and 6

--- cricketApp.js
+++ cricketApp.js
@@ -1,6 +1,6 @@
 var readline = require('readline');
 const Match = require('./match');
-const {Player, Bowlers} = require('./player');
+const { Batsman, Bowler } = require('./player');
 
 
 const rl = readline.createInterface({
@@ -9,29 +9,51 @@ const rl = readline.createInterface({
 });
 
 rl.on('line', (line) => {
-    var arr = [];
     var nums = line.split(' ');
     var target = parseInt(nums[0]);
     var overs = parseInt(nums[1]);
     var batmansType = parseInt(nums[2]);
-    play(target, overs, batmansType);
+    var bowlerType = parseInt(nums[3]);
+    play(target, overs, batmansType, bowlerType);
 });
 
 
 
 
-function getOverallScore(balls, batmansType) {
+function getOverallScore(balls, batmansType, bowlerType) {
     let bowlersRun;
     let batsmanRun;
+    let wicketBowler = false;
+    let tailEnderBatsman = false;
     let score = 0;
     for (let i = 0; i < balls; i++) {
-        bowlersRun = new Bowlers().getRun();
-        console.log("Bowler",bowlersRun);
-        batsmanRun = new Player(batmansType).getRun();
-        console.log("Batsman",batsmanRun)
-        if (batsmanRun === bowlersRun) {
-            return -1;
+        bowlersRun = new Bowler(bowlerType).getRun();
+        // wicketBowler possess 'true' for NormalBowler(geting wickets)
+        // 'false' for PartTimeBowler (No wicket )
+        wicketBowler = new Bowler(bowlerType).getwicket();
+        console.log("Bowler", bowlersRun);
+
+        tailEnderBatsman = new Batsman(batmansType).isTailEndBatsman();
+        batsmanRun = new Batsman(batmansType).getRun();
+
+        console.log("Batsman", batsmanRun)
+        
+        // Normal Bowler
+        if (wicketBowler) {
+            if (batsmanRun === bowlersRun) { // Others BAtsman (include tail)
+                return score;
+            } 
+            if(tailEnderBatsman){  // TailEndBatsman 
+                let oddBowlerRun = (bowlersRun % 2 !== 0 )? 'true': 'false';
+                let oddBatsmanRun = (batsmanRun % 2 !== 0 )? 'true': 'false';
+                if((oddBatsmanRun && oddBowlerRun ) || (!oddBowlerRun && !oddBatsmanRun)){
+                    return score; 
+                }
+            }
+        } else {  // partTime
+            continue;
         }
+
         if (score >= this.target) {
             return score;
         }
@@ -42,10 +64,10 @@ function getOverallScore(balls, batmansType) {
 
 }
 
-function play(target, overs, batmansType) {
+function play(target, overs, batmansType, bowlerType) {
     let match = new Match(target, overs);
     let balls = match.balls(overs);
-    let totalScores = getOverallScore(balls, batmansType);
+    let totalScores = getOverallScore(balls, batmansType, bowlerType);
     if (totalScores >= target) {
         console.log("Batsman won");
     } else {
--- player.js
+++ player.js
@@ -1,4 +1,5 @@
-class Player {
+// Batsman
+class Batsman {
     constructor(type) {
         this.type = type;
         this.run = 0;
@@ -11,42 +12,79 @@ class Player {
             return new HitBatsman().getRun();
         } else if (this.type === 2) {
             return new DefensiverBatsman().getRun();
-        } else {
+        } else if(this.type === 3){
+            return new TailEnderBatsman().getRun();
+        }
+        else {
             console.log("wrong");
             return;
         }
     }
+    // isOut() {
+    //     if(this.type === 3){
+    //         return new TailEnderBatsman().isOut();
+    //     }
+    //     else {
+    //         return new NormalBatsman().isOut();
+
+    //     }
+    // }
+    isTailEndBatsman(){
+        return (this.type === 3)? 'true' : 'false';
+    }
 
 }
-class NormalBatsman extends Player {
+class NormalBatsman extends Batsman {
     getRun (){
         let min = 0;
         let max = 7;
         return Math.floor(Math.random() * (max - min)) + min;
     }
 }
-class HitBatsman extends Player {
+class HitBatsman extends Batsman {
     getRun() {
-        console.log("Hit")
         let numbers = [0, 4, 6];
         return numbers[Math.floor(Math.random() * numbers.length)];
     }
 }
-class Bowlers  {
-    constructor() {
-        this.max = 7;
-        this.min = 0;
-    }
-    getRun() {
-        console.log("bowler");
-        return Math.floor(Math.random() * (this.max - this.min)) + this.min;
-    }
-}
-class DefensiverBatsman extends Player {
+
+
+class DefensiverBatsman extends Batsman {
     getRun() {
         let numbers = [0, 1, 2, 3];
         return numbers[Math.floor(Math.random() * numbers.length)];
     }
 }
+class TailEnderBatsman extends Batsman {
+    getRun (){
+        let min = 0;
+        let max = 7;
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+    isOut(){
 
-module.exports = {Player, Bowlers}
+    }
+}
+
+// Bowler
+
+class Bowler {
+    constructor(type) {
+        this.max = 7;
+        this.min = 0;
+        this.type = type;
+    }
+    
+    getRun() {
+        return Math.floor(Math.random() * (this.max - this.min)) + this.min;
+    }
+    getwicket() {
+        if(this.type === 0){
+            return true;
+        } else if(this.type === 1){
+            return false;
+        }
+    }
+}
+
+module.exports = {Batsman, Bowler}
